60700758a82b082fdba4ba58b8c549afede07ba8
RuefulMage <feonorabd2000@gmail.com>
2020-06-13 19:24:56 +0300

    links to other pages added to index page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,75 @@
-import './assets/scss/fonts.scss'
-import './assets/scss/style-delete.scss'
+import 'nouislider/distribute/nouislider.css';
+import './assets/scss/fonts.scss';
+import './assets/scss/style-delete.scss';
+import './assets/scss/colors.scss';
+import './assets/scss/common.scss';
+import 'air-datepicker/src/sass/datepicker.scss';
+import 'air-datepicker/src/sass/navigation.scss';
+import 'air-datepicker/src/sass/cell.scss';
+import 'air-datepicker/src/sass/timepicker.scss';
+
+import './pages/index/index.scss';
+import './pages/forms-elements/forms-elements.scss';
+import './pages/colors-and-fonts/colors-and-fonts.scss';
+import './pages/cards/cards.scss';
+import './pages/headers-and-footers/headers-and-footers.scss';
+import './pages/registration/registration.scss';
+import './pages/sign-in/sign-in.scss';
+import './pages/room-details/room-details.scss'
+import './pages/search/search.scss';
+import './pages/home/home.scss';
+
+
+import './blocks/header/header.scss';
+import './blocks/header-navigation/header-navigation.scss';
+import './blocks/footer/footer.scss';
+import './blocks/footer-navigation/footer-navigation.scss';
+import './blocks/copyright/copyright.scss';
+import './blocks/textfield/textfield.scss';
+import './blocks/footer-creative/footer-creative.scss';
+
+import './blocks/dropdown/dropdown.scss';
+import './blocks/date-dropdown/date-dropdown.scss';
+import './blocks/subscription-textfiled/subscription-textfield.scss';
+import './blocks/expandable-checkbox-list/expandable-checkbox-list.scss';
+import './blocks/checkbox/checkbox.scss';
+import './blocks/icon-text/icon-text.scss';
+import './blocks/range-slider/range-slider.scss';
+import './blocks/radio-button/radio-button.scss';
+import './blocks/toggle/toggle.scss';
+import './blocks/like-button/like-button.scss';
+import './blocks/rate-button/rate-button.scss';
+import './blocks/link-button/link-button.scss';
+import './blocks/link/link.scss';
+import './blocks/form-button/form-button.scss';
+import './blocks/pagination/pagination.scss';
+import './blocks/rich-checkbox/rich-checkbox.scss';
+import './blocks/bullet-list/bullet-list.scss';
+import './blocks/bullet/bullet.scss';
+import './blocks/comment/comment.scss';
+import './blocks/logo/logo.scss';
+import './blocks/color-description/color-description.scss';
+import './blocks/text-type/text-type.scss';
+import './blocks/calendar/calendar.scss';
+import './blocks/logo-with-text/logo-with-text.scss';
+import './blocks/social-net-icon/social-net-icon.scss';
+import './blocks/pie-chart/pie-chart.scss';
+
+import './blocks/find-apartment-card/find-apartment-card.scss';
+import './blocks/apartment-info/apartment-info.scss';
+import './blocks/apartment-booking-card/apartment-booking-card.scss';
+import './blocks/registration-card/registration-card.scss';
+import './blocks/signin-card/signin-card.scss';
+import './blocks/apartment-description-card/apartment-description-card.scss';
+
+
+import './blocks/dropdown/dropdown';
+import './blocks/textfield/textfield';
+import './blocks/date-dropdown/date-dropdown';
+import './blocks/expandable-checkbox-list/expandable-checkbox-list';
+import './blocks/range-slider/range-slider';
+import './blocks/like-button/like-button';
+import './blocks/pagination/pagination';
+import './blocks/calendar/calendar';
+import './blocks/apartment-description-card/apartment-description-card'
+import './blocks/pie-chart/pie-chart';
