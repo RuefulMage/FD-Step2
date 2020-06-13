4185f62c4e9898b1bf9af62b70adba07945ac5df
RuefulMage <feonorabd2000@gmail.com>
2020-06-13 19:19:41 +0300

    some bugs fixed

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -101,3 +101,10 @@ let dateDropdownsList = document.getElementsByClassName('js-date-dropdown');
 for(let i = 0; i < dateDropdownsList.length; i++){
     createCalendar(dateDropdownsList[i]);
 }
+
+let calendarList = document.getElementsByClassName('calendar');
+console.log(calendarList);
+
+for(let i = 0; i < calendarList.length; i++){
+    createCalendar(calendarList[i]);
+}
