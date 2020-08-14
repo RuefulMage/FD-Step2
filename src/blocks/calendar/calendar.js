12033794df8136527fbd9ad8a4316a7954ecc5ca
RuefulMage <feonorabd2000@gmail.com>
2020-08-14 17:01:55 +0300

    dropdown and calendar blocks changed by fsd-standarts

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -2,17 +2,22 @@ import 'air-datepicker';
 
 
 function createCalendar(node){
-    let dateDropdownInputs = node.querySelectorAll('.js-calendar');
+    const calendarClass = 'js-calendar';
+    const clearButtonClass = 'calendar__clear-button';
+    const appendButtonClass = 'calendar__append-button';
+    const buttonsWrapperClass = 'calendar__buttons-wrapper';
 
+    let dateDropdownInputs = node.querySelectorAll('.' + calendarClass);
+
+    //иинициализация плагина
     let calendar = $(dateDropdownInputs[0]).datepicker({
-        // minDate: new Date(),
         multipleDatesSeparator: " - ",
         range: true,
         prevHtml: "<div class='calendar__arrow'>arrow_back</div>",
         nextHtml: "<div class='calendar__arrow'>arrow_forward</div>",
         onSelect: function(_, date){
             this.dates = date;
-            clearInputs(dateDropdownInputs);
+            clearInputs();
         },
         navTitles: {
             days: 'MM yyyy'
@@ -22,89 +27,85 @@ function createCalendar(node){
 
     let calendarData = calendar.data('datepicker');
 
-    if(dateDropdownInputs.length == 2){
-        dateDropdownInputs[1].addEventListener('click', showCalendar.bind(null, calendarData));
+    // Навешивание на инпут обработчик события click
+    if(dateDropdownInputs.length === 2){
+        dateDropdownInputs[1].addEventListener('click', showCalendar);
     }
+
+    // Создание кнопок применить и очистить и навешивание на них обработчиков событий
     createCalendarButtonsBlock("очистить",
         "применить", calendarData, dateDropdownInputs);
-}
 
-// let calendarBlock = document.getElementsByClassName('date-dropdown-with-plugin')[0];
-// createCalendar(calendarBlock);
 
-function showCalendar(calendarData){
-    calendarData.show();
-}
 
-function clearInputs(dateDropdownInputs){
-    for(let i = 0; i < dateDropdownInputs.length; i++){
-        dateDropdownInputs[i].value = '';
+
+    // Ф-я, показывающая календарь
+    function showCalendar(){
+        calendarData.show();
     }
-}
 
-function createCalendarButtonsBlock(clearButtonName, appendButtonName,
-                                    calendarData, dateDropdownInputs){
-    let clearButton = document.createElement('button');
-    clearButton.innerText = clearButtonName;
-    clearButton.classList.add("calendar__clear-button");
-
-    let appendButton = document.createElement('button');
-    appendButton.innerText = appendButtonName;
-    appendButton.classList.add("calendar__append-button");
-    
-    addButtonsHandlers(clearButton, appendButton, calendarData,
-        dateDropdownInputs);
-
-    let calendarButtonsBlock = document.createElement('div');
-    calendarButtonsBlock.classList.add("calendar__buttons-wrapper");
-    calendarButtonsBlock.append(clearButton);
-    calendarButtonsBlock.append(appendButton);
-
-    let calendarDomElement = calendarData.$datepicker;
-    calendarDomElement.append(calendarButtonsBlock);
-}
-
-let mounthsNames = new Map([
-    [ 0, 'янв'],[ 1, 'фев'],
-    [ 2, 'мар'],[ 3, 'апр'],
-    [ 4, 'май'],[ 5, 'июн'],
-    [ 6, 'июл'],[ 7, 'авг'],
-    [ 8, 'сен'],[ 9, 'окт'],
-    [ 10, 'ноб'],[ 11, 'дек']
-])
-
-function addButtonsHandlers(clearButton, appendButton,
-                            calendarData, dateDropdownInputs){
-    clearButton.addEventListener('click', function () {
-        calendarData.clear();
-    });
-
-
-    appendButton.addEventListener('click', function () {
-        calendarData.hide();
-        if(dateDropdownInputs.length == 1){
-            // dateDropdownInputs[0].value= calendarData.selectedDates[0].toLocaleDateString()
-            //     + " - " + calendarData.selectedDates[1].toLocaleDateString();
-            dateDropdownInputs[0].value = calendarData.selectedDates[0].getDate() + " "
-                + mounthsNames.get(calendarData.selectedDates[0].getMonth()) + " - "
-                + calendarData.selectedDates[1].getDate() + " "
-                + mounthsNames.get(calendarData.selectedDates[1].getMonth())
-        } else {
-            dateDropdownInputs[0].value= calendarData.selectedDates[0].toLocaleDateString();
-            dateDropdownInputs[1].value= calendarData.selectedDates[1].toLocaleDateString();
+    // Функция, очищающая все инпуты, привязанные к календарю
+    function clearInputs(){
+        for(let i = 0; i < dateDropdownInputs.length; i++){
+            dateDropdownInputs[i].value = '';
         }
-    })
+    }
+
+
+    // Функция, создающая DOM-элементы кнопок и добавляющая их в документ
+    function createCalendarButtonsBlock(clearButtonName, appendButtonName){
+        let clearButton = document.createElement('button');
+        clearButton.innerText = clearButtonName;
+        clearButton.classList.add(clearButtonClass);
+
+        let appendButton = document.createElement('button');
+        appendButton.innerText = appendButtonName;
+        appendButton.classList.add(appendButtonClass);
+
+        addButtonsHandlers(clearButton, appendButton, calendarData,
+            dateDropdownInputs);
+
+        let calendarButtonsBlock = document.createElement('div');
+        calendarButtonsBlock.classList.add(buttonsWrapperClass);
+        calendarButtonsBlock.append(clearButton);
+        calendarButtonsBlock.append(appendButton);
+
+        let calendarDomElement = calendarData.$datepicker;
+        calendarDomElement.append(calendarButtonsBlock);
+    }
+
+
+    let mounthsNames = new Map([
+        [ 0, 'янв'],[ 1, 'фев'],
+        [ 2, 'мар'],[ 3, 'апр'],
+        [ 4, 'май'],[ 5, 'июн'],
+        [ 6, 'июл'],[ 7, 'авг'],
+        [ 8, 'сен'],[ 9, 'окт'],
+        [ 10, 'ноб'],[ 11, 'дек']
+    ]);
+
+    // Функция, навешивающая обработчики событий на кнопки очистить и применить
+    function addButtonsHandlers(clearButton, appendButton,
+                                calendarData, dateDropdownInputs){
+        clearButton.addEventListener('click', function () {
+            calendarData.clear();
+        });
+
+
+        appendButton.addEventListener('click', function () {
+            calendarData.hide();
+            if(dateDropdownInputs.length == 1){
+                dateDropdownInputs[0].value = calendarData.selectedDates[0].getDate() + " "
+                    + mounthsNames.get(calendarData.selectedDates[0].getMonth()) + " - "
+                    + calendarData.selectedDates[1].getDate() + " "
+                    + mounthsNames.get(calendarData.selectedDates[1].getMonth())
+            } else {
+                dateDropdownInputs[0].value= calendarData.selectedDates[0].toLocaleDateString();
+                dateDropdownInputs[1].value= calendarData.selectedDates[1].toLocaleDateString();
+            }
+        });
+    }
+
 }
 
-let dateDropdownsList = document.getElementsByClassName('js-date-dropdown');
-
-for(let i = 0; i < dateDropdownsList.length; i++){
-    createCalendar(dateDropdownsList[i]);
-}
-
-let calendarList = document.getElementsByClassName('calendar');
-console.log(calendarList);
-
-for(let i = 0; i < calendarList.length; i++){
-    createCalendar(calendarList[i]);
-}
+export default createCalendar;
--- src/blocks/date-dropdown/date-dropdown.js
+++ src/blocks/date-dropdown/date-dropdown.js
@@ -1,2 +1,14 @@
-import '../calendar/calendar';
-import '../calendar/calendar.scss';
+import createCalendar from "../calendar/calendar";
+
+
+let dateDropdownsList = document.getElementsByClassName('js-date-dropdown');
+
+for(let i = 0; i < dateDropdownsList.length; i++){
+    createCalendar(dateDropdownsList[i]);
+}
+
+let calendarList = document.getElementsByClassName('calendar');
+
+for(let i = 0; i < calendarList.length; i++){
+    createCalendar(calendarList[i]);
+}
