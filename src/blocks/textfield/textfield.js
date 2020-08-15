41791c702bba02688b530727703c24a65ee6580c
RuefulMage <feonorabd2000@gmail.com>
2020-08-15 21:22:31 +0300

    textfield block changed according to FSD standarts

--- src/blocks/textfield/textfield.js
+++ src/blocks/textfield/textfield.js
@@ -1,7 +1,3 @@
-//TODO добавить валидацию поля
-
 import 'jquery-mask-plugin';
 
-jQuery(document).ready(function () {
-    $('.js-masked').mask('00.00.0000');
-})
+$('.js-masked').mask('00.00.0000');
