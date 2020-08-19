c44b7efc03cfb57a13f7344ce838b6914e43f34c
RuefulMage <feonorabd2000@gmail.com>
2020-08-19 14:02:35 +0300

    search js init added

---
+++ src/pages/search/search.js
@@ -0,0 +1,39 @@
+function initShowFormButton(buttonElement) {
+    const filterClass = 'js-search__filter-column';
+    const filterHiddenClass = 'search__filter-column_hidden';
+
+    let filterElement = buttonElement.parentElement
+        .querySelector('.' + filterClass);
+
+    buttonElement.addEventListener('click', buttonClickHandler);
+    if(document.body.clientWidth <= 880){
+        filterElement.classList.add(filterHiddenClass);
+    }
+    filterResizeHandler(filterElement)
+
+    function buttonClickHandler(){
+        if(filterElement.classList.contains(filterHiddenClass)){
+            filterElement.classList.remove(filterHiddenClass);
+        } else {
+            filterElement.classList.add(filterHiddenClass);
+        }
+    }
+}
+
+function filterResizeHandler(filterElement) {
+    window.addEventListener('resize', resizeHandler);
+
+    function resizeHandler() {
+        const filterHiddenClass = 'search__filter-column_hidden';
+
+        if (document.body.clientWidth >= 880){
+            filterElement.classList.remove(filterHiddenClass);
+        }
+    }
+}
+
+let buttonsList = document.getElementsByClassName('js-search__show-filter-button');
+
+for (let i = 0; i < buttonsList.length; i++) {
+    initShowFormButton(buttonsList[i]);
+}
