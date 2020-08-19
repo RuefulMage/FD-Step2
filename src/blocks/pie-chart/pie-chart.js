c426c8f0b421e7a42b6315e3dc31a8a4cd712109
RuefulMage <feonorabd2000@gmail.com>
2020-08-19 14:01:28 +0300

    pie chart block refactored and changed according to FSD standarts

--- src/blocks/pie-chart/pie-chart.js
+++ src/blocks/pie-chart/pie-chart.js
@@ -1,35 +1,80 @@
-let element = document.getElementsByClassName('pie-chart')[0];
-let values = JSON.parse(element.getAttribute('data-values'));
-let {great, bad, good, normal} = values;
-let sum = great + bad + good + normal;
-let center = element.querySelector('.pie-chart__sum');
-center.innerText = sum;
+// let element = document.getElementsByClassName('pie-chart')[0];
+// let values = JSON.parse(element.getAttribute('data-values'));
+// let {great, bad, good, normal} = values;
+// let sum = great + bad + good + normal;
+// let center = element.querySelector('.pie-chart__sum');
+// center.innerText = sum;
+//
+//
+// let amountInProcent = sum / 100;
+// let halfsBetweenMargin = (((great + bad) === 0) || ((good + normal) === 0)) ? 0 : 1.6;;
+// let leftHalfInProcent = (great / amountInProcent) + (bad / amountInProcent) - halfsBetweenMargin/2;
+// let rightHalfInProcent = 100 - leftHalfInProcent - halfsBetweenMargin/2;
+// element.querySelector('.pie-chart__first-half').style.width = leftHalfInProcent + '%';
+// element.querySelector('.pie-chart__second-half').style.width = rightHalfInProcent + '%';
+//
+//
+//
+//
+// let greatAndBadBetweenMargin = ((great === 0) || (bad === 0)) ? 0 : 1.6;
+//
+// let rightHalfSum = great + bad;
+// let rightHalfAmountInProcent = rightHalfSum / 100;
+// let greatPartOfHalf = (great / rightHalfAmountInProcent) - greatAndBadBetweenMargin/2;
+// let badPartOfHalf = (bad / rightHalfAmountInProcent) - greatAndBadBetweenMargin/2;
+// element.querySelector('.pie-chart__great').style.height = greatPartOfHalf + '%';
+// element.querySelector('.pie-chart__bad').style.height = badPartOfHalf + '%';
+//
+//
+// let goodAndNormalBetweenMargin = ((good === 0) || (normal === 0)) ? 0 : 1.6;
+// let leftHalfSum = good + normal;
+// let leftHalfAmountInProcent = leftHalfSum / 100;
+// let normalPartOfHalf = (normal / leftHalfAmountInProcent) - goodAndNormalBetweenMargin/2;
+// let goodPartOfHalf = (good / leftHalfAmountInProcent) - goodAndNormalBetweenMargin/2;
+// element.querySelector('.pie-chart__good').style.height = goodPartOfHalf + '%';
+// element.querySelector('.pie-chart__normal').style.height = normalPartOfHalf + '%';
+
+function initPieChart(chartElement) {
+    const sumClass = 'js-pie-chart__sum';
+    const firstHalfClass = 'js-pie-chart__first-half';
+    const secondHalfClass = 'js-pie-chart__second-half';
+    const greatClass = 'js-pie-chart__great';
+    const badClass = 'js-pie-chart__bad';
+    const goodClass = 'js-pie-chart__good';
+    const normalClass = 'js-pie-chart__normal';
+
+    let values = JSON.parse(chartElement.getAttribute('data-values'));
+    let {great, bad, good, normal} = values;
+    let sum = great + bad + good + normal;
+    let center = chartElement.querySelector('.' + sumClass);
+
+    center.innerText = sum;
+
+    let amountInProcent = sum / 100;
+    let halfsBetweenMargin = (((great + bad) === 0) || ((good + normal) === 0)) ? 0 : 1.6;
+    let leftHalfInProcent = (great / amountInProcent) + (bad / amountInProcent) - halfsBetweenMargin/2;
+    let rightHalfInProcent = 100 - leftHalfInProcent - halfsBetweenMargin/2;
+    chartElement.querySelector('.' + firstHalfClass).style.width = leftHalfInProcent + '%';
+    chartElement.querySelector('.' + secondHalfClass).style.width = rightHalfInProcent + '%';
 
 
-let amountInProcent = sum / 100;
-let halfsBetweenMargin = (((great + bad) == 0) || ((good + normal) == 0)) ? 0 : 1.6;;
-let leftHalfInProcent = (great / amountInProcent) + (bad / amountInProcent) - halfsBetweenMargin/2;
-let rightHalfInProcent = 100 - leftHalfInProcent - halfsBetweenMargin/2;
-element.querySelector('.pie-chart__first-half').style.width = leftHalfInProcent + '%';
-element.querySelector('.pie-chart__second-half').style.width = rightHalfInProcent + '%';
+    let greatAndBadBetweenMargin = ((great === 0) || (bad === 0)) ? 0 : 1.6;
+
+    let rightHalfSum = great + bad;
+    let rightHalfAmountInProcent = rightHalfSum / 100;
+    let greatPartOfHalf = (great / rightHalfAmountInProcent) - greatAndBadBetweenMargin/2;
+    let badPartOfHalf = (bad / rightHalfAmountInProcent) - greatAndBadBetweenMargin/2;
+    chartElement.querySelector('.' + greatClass).style.height = greatPartOfHalf + '%';
+    chartElement.querySelector('.' + badClass).style.height = badPartOfHalf + '%';
 
 
+    let goodAndNormalBetweenMargin = ((good === 0) || (normal === 0)) ? 0 : 1.6;
+    let leftHalfSum = good + normal;
+    let leftHalfAmountInProcent = leftHalfSum / 100;
+    let normalPartOfHalf = (normal / leftHalfAmountInProcent) - goodAndNormalBetweenMargin/2;
+    let goodPartOfHalf = (good / leftHalfAmountInProcent) - goodAndNormalBetweenMargin/2;
+    chartElement.querySelector('.' + goodClass).style.height = goodPartOfHalf + '%';
+    chartElement.querySelector('.' + normalClass).style.height = normalPartOfHalf + '%';
+}
 
-
-let greatAndBadBetweenMargin = ((great == 0) || (bad == 0)) ? 0 : 1.6;
-
-let rightHalfSum = great + bad;
-let rightHalfAmountInProcent = rightHalfSum / 100;
-let greatPartOfHalf = (great / rightHalfAmountInProcent) - greatAndBadBetweenMargin/2;
-let badPartOfHalf = (bad / rightHalfAmountInProcent) - greatAndBadBetweenMargin/2;
-element.querySelector('.pie-chart__great').style.height = greatPartOfHalf + '%';
-element.querySelector('.pie-chart__bad').style.height = badPartOfHalf + '%';
-
-
-let goodAndNormalBetweenMargin = ((good == 0) || (normal == 0)) ? 0 : 1.6;
-let leftHalfSum = good + normal;
-let leftHalfAmountInProcent = leftHalfSum / 100;
-let normalPartOfHalf = (normal / leftHalfAmountInProcent) - goodAndNormalBetweenMargin/2;
-let goodPartOfHalf = (good / leftHalfAmountInProcent) - goodAndNormalBetweenMargin/2;
-element.querySelector('.pie-chart__good').style.height = goodPartOfHalf + '%';
-element.querySelector('.pie-chart__normal').style.height = normalPartOfHalf + '%';
+export default initPieChart;
