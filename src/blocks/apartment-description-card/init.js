f6a5aa0a955a20dbdf6b26c40228c7d95a45a5ce
RuefulMage <feonorabd2000@gmail.com>
2021-05-09 00:04:22 +0300

    little bugs fixed in apartment-booking-card, apartment-description-card and apartment-info blocks #36

--- src/blocks/apartment-description-card/init.js
+++ src/blocks/apartment-description-card/init.js
@@ -1,6 +1,4 @@
 import ImagesSlider from './ImagesSlider';
 
 const sliderList = document.getElementsByClassName('js-apartment-description-card__slider');
-for (let i = 0; i < sliderList.length; i += 1) {
-  new ImagesSlider(sliderList[i]);
-}
+Array.from(sliderList).forEach(slider => new ImagesSlider(slider));
