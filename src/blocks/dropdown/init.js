a4960471e298f9024addd9bd0d0e3250a94aa31e
RuefulMage <feonorabd2000@gmail.com>
2021-05-24 14:27:19 +0300

    fixed parameters of dropdown in all files, where dropdown is used #36

--- src/blocks/dropdown/init.js
+++ src/blocks/dropdown/init.js
@@ -2,10 +2,10 @@ import Dropdown from './Dropdown';
 
 const dropdownsWithButtonsList = document.getElementsByClassName('js-dropdown_with-buttons');
 for (let i = 0; i < dropdownsWithButtonsList.length; i += 1) {
-  new Dropdown(dropdownsWithButtonsList[i], true, true);
+  new Dropdown(dropdownsWithButtonsList[i], true);
 }
 
 const dropdownsList = document.getElementsByClassName('js-dropdown');
 for (let i = 0; i < dropdownsList.length; i += 1) {
-  new Dropdown(dropdownsList[i], false, false);
+  new Dropdown(dropdownsList[i], false);
 }
