c9c967f94c8941eb2f467c6dfab3a803b10e5988
RuefulMage <feonorabd2000@gmail.com>
2021-06-08 00:34:51 +0300

    imported sign in card styles to sign in page #36

--- src/pages/sign-in/sign-in.js
+++ src/pages/sign-in/sign-in.js
@@ -13,5 +13,9 @@ import '../../blocks/footer-navigation/footer-navigation.scss';
 import '../../blocks/title/title.scss';
 import '../../blocks/copyright/copyright.scss';
 import '../../blocks/social-net-icon/social-net-icon.scss';
+import '../../blocks/signin-card/signin-card.scss';
+import '../../blocks/textfield/textfield.scss';
+import '../../blocks/form-button/form-button.scss';
 
 import '../../blocks/header/init';
+import '../../blocks/textfield/textfield';
